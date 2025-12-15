d638c103b3c1504fa534b647c359cf281d5d114a
Seulbae Kim <seulbae-security@gmail.com>
2025-12-15 10:21:45 +0900

    add setup_zsh.sh

---
+++ setup_zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo apt update
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+wget -O ~/.zshrc https://raw.githubusercontent.com/postech-compsec/rc/refs/heads/master/zshrc
+wget -O ~/.spaceshiprc https://raw.githubusercontent.com/postech-compsec/rc/refs/heads/master/spaceshiprc
+echo "Changing default shell to zsh..."
+chsh -s $(which zsh)
+echo "Done. Login again!"
