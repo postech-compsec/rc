8e4b1cdb7ed7fb84482d89f0b1cf8e70c2dcbb27
Seulbae Kim <seulbae-security@gmail.com>
2025-12-15 12:08:43 +0900

    add setup_tmux.sh

---
+++ setup_tmux.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O ~/.tmux.conf https://raw.githubusercontent.com/postech-compsec/rc/refs/heads/master/tmux.conf
+wget -O ~/.ssh/rc https://raw.githubusercontent.com/postech-compsec/rc/refs/heads/master/sshrc
