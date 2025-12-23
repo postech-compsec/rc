d852e2cac72c76b14d2171196f06753b176211fc
Seulbae Kim <seulbae.kim@gmail.com>
2025-12-23 12:03:19 +0900

    create symlink

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -5,6 +5,7 @@ sudo apt update
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 wget -O ~/.zshrc https://raw.githubusercontent.com/postech-compsec/rc/refs/heads/master/zshrc
